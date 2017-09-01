347ef82970c19dc0028adb9082cf2f5095b2c62f
misaka4e21 <misaka4e21@gmail.com>
2017-09-01 09:09:53 +0800

    Add image viewer.

---
+++ app/src/main/java/io/github/alphacalculus/alphacalculus/ImageViewActivity.java
@@ -0,0 +1,17 @@
+package io.github.alphacalculus.alphacalculus;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class ImageViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_view);
+        int resid = getIntent().getIntExtra("io.github.alphacalculus.IMAGE_SHOW_ID", 0);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(resid);
+    }
+}
--- app/src/main/java/io/github/alphacalculus/alphacalculus/TextViewWithImages.java
+++ app/src/main/java/io/github/alphacalculus/alphacalculus/TextViewWithImages.java
@@ -1,10 +1,16 @@
 package io.github.alphacalculus.alphacalculus;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
 import android.support.v7.widget.AppCompatTextView;
 import android.text.Spannable;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.text.style.ImageSpan;
 import android.util.AttributeSet;
+import android.view.View;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -25,13 +31,14 @@ public class TextViewWithImages extends AppCompatTextView {
 
     @Override
     public void setText(CharSequence text, BufferType type) {
-        Spannable s = getTextWithImages(getContext(), text);
+        Spannable s = getTextWithImages(getContext(), text, getActivity());
+        super.setMovementMethod(LinkMovementMethod.getInstance());
         super.setText(s, BufferType.SPANNABLE);
     }
 
     private static final Spannable.Factory spannableFactory = Spannable.Factory.getInstance();
 
-    private static boolean addImages(Context context, Spannable spannable) {
+    private static boolean addImages(Context context, Spannable spannable, final Activity parent) {
         Pattern refImg = Pattern.compile("\\Q[img src=\\E([a-zA-Z0-9_]+?)\\Q/]\\E");
         boolean hasChanges = false;
 
@@ -48,8 +55,8 @@ public class TextViewWithImages extends AppCompatTextView {
                     break;
                 }
             }
-            String resname = spannable.subSequence(matcher.start(1), matcher.end(1)).toString().trim();
-            int id = context.getResources().getIdentifier(resname, "drawable", context.getPackageName());
+            final String resname = spannable.subSequence(matcher.start(1), matcher.end(1)).toString().trim();
+            final int id = context.getResources().getIdentifier(resname, "drawable", context.getPackageName());
             if (set) {
                 hasChanges = true;
                 spannable.setSpan(new ImageSpan(context, id),
@@ -57,15 +64,37 @@ public class TextViewWithImages extends AppCompatTextView {
                         matcher.end(),
                         Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
                 );
+                spannable.setSpan(new ClickableSpan() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(parent, ImageViewActivity.class);
+                        intent.putExtra("io.github.alphacalculus.IMAGE_SHOW_ID", id);
+                        parent.startActivity(intent);
+                    }
+                }, matcher.start(), matcher.end(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
             }
         }
 
         return hasChanges;
     }
 
-    private static Spannable getTextWithImages(Context context, CharSequence text) {
+    private Activity getActivity() {
+        Context context = getContext();
+        if (context instanceof Activity) {
+            return (Activity) context;
+        }
+        while (context instanceof ContextWrapper) {
+            if (context instanceof Activity) {
+                return (Activity) context;
+            }
+            context = ((ContextWrapper) context).getBaseContext();
+        }
+        return null;
+    }
+
+    private static Spannable getTextWithImages(Context context, CharSequence text, Activity parent) {
         Spannable spannable = spannableFactory.newSpannable(text);
-        addImages(context, spannable);
+        addImages(context, spannable, parent);
         return spannable;
     }
 }
